1c91f1c715da746c360da8dff03b064c521a4131
Tessa Pierce <tesssapierce@gmail.com>
2020-09-29 16:11:09 -0600

    Updating questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,13 +21,33 @@ var displayCount = document.querySelector(".countdown");
 var questionDisplay = document.querySelector(".question");
 var ulTag = document.querySelector(".answers");
 var idx = 0;
+var button = document.querySelectorAll(".button")
+
+// var idx = 0;
+var letter = ["A", "B", "C", "D"];
+console.log(letter[0])
 
 var quiz = [
-  {question: "This is the first question", choices: [
-    "this is the first answer", "this is the second answer", "this is the third answer", "this is the fourth answer"], answer: "this is the correct answer"},
-  {question: "This is the second question", choices: [
-    "this is the first answer", "this is the second answer", "this is the third answer", "this is the fourth answer"], answer: "this is the correct answer"}
-  ]
+  {
+    question: "The # symbol specifies that the selector is...", 
+    choices: ["Class", "Number", "Tag", "ID"], 
+    answer: "."
+  },{
+    question: "Where is the correct place to put the title tag in an HTML document?", 
+    choices: ["Above the HTML tag", "In the body of the document", "In the head of the document", "It doesn't matter"], 
+    answer: "In the head of the document"
+  },{
+    question: "Items in a ____ list are preceded by numbers", 
+    choices: ["unordered", "ordered", "bulleted", "grocery"], 
+    answer: "ordered"
+  },{
+    question: "What does CSS stand for?", 
+    choices: ["Custom Style Sheets", "Colorful Style Sheets", "Computer Style Sheets", "Cascading Style Sheets"], 
+    answer: "Cascading Style Sheets"
+  }
+];
+
+  console.log(quiz.length)
 
 //Start Timer and unhide quiz
 function startQuiz(e){
@@ -54,11 +74,27 @@ function startCountdown(){
 
 //Questions and Answers populating
 function populateQA(idx){
-  for (var i=0; i < quiz[idx].choices.length; i++){
-  questionDisplay.textContent = "Question: " + quiz[idx].question;
-  var answer1 = document.createElement("li");
-  answer1.textContent = quiz[idx].choices[i]
-  ulTag.appendChild(answer1)}
+  for (var i=0; i<quiz.length; i++){
+    for (var i=0; i < quiz[idx].choices.length; i++){
+    
+    //Question display
+    questionDisplay.textContent = "Question: " + quiz[idx].question;
+
+    //Answer display
+    var button = document.createElement("button");
+    button.textContent = letter[i]
+    ulTag.appendChild(button);
+    button.setAttribute("class", "button");
+    var answer = document.createElement("li");
+    answer.textContent = quiz[idx].choices[i];
+    ulTag.appendChild(answer);
+    }
+  }
+  button.addEventListener("click", logIt);
+}
+
+function logIt(){
+  console.log("click");
 }
 
 //Starts everything by clicking "Start Quiz"
